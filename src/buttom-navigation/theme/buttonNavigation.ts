0f1db45d3b725408f25e73756503f89217c52e9f
kuba <vike123@foxmail.com>
2022-09-04 11:18:50 +0800

    feat: nav component theme

---
+++ src/buttom-navigation/theme/buttonNavigation.ts
@@ -0,0 +1,17 @@
+const buttonNavigationTheme = {
+  '--bottom-navigation-height': '50px',
+  '--bottom-navigation-z-index': '1',
+  '--bottom-navigation-background-color': '#fff',
+  '--bottom-navigation-border-color': '#bcc2cb',
+  '--bottom-navigation-fab-offset': '4px',
+
+  '--bottom-navigation-item-font-size': '3.2vw',
+  '--bottom-navigation-item-inactive-color': '#646566',
+  '--bottom-navigation-item-active-color': '#3a7afe',
+  '--bottom-navigation-item-active-background-color': '#fff',
+  '--bottom-navigation-item-line-height': '1',
+  '--bottom-navigation-item-icon-size': '22px',
+  '--bottom-navigation-item-icon-margin-bottom': '5px',
+}
+
+export default buttonNavigationTheme
---
+++ src/tabs/theme/tabs.ts
@@ -0,0 +1,16 @@
+const TabsTheme = {
+ ' --tabs-item-horizontal-height': '44px',
+  '--tabs-item-vertical-height': '66px',
+  '--tabs-radius': '2px',
+  '--tabs-padding': '12px',
+  '--tabs-indicator-size': '2px',
+ ' --tabs-indicator-background': '#3a7afe',
+  '--tabs-background': '#fff',
+  '--tab-padding': '12px',
+  '--tab-active-color': '#3a7afe',
+  '--tab-inactive-color': '#646566',
+  '--tab-disabled-color': '#aaa',
+  '--tab-font-size': '3.733333vw',
+}
+
+export default TabsTheme
