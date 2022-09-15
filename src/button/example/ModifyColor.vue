f1932f92f336fb0111414ecb16ef851de3becf13
kuba <vike123@foxmail.com>
2022-09-15 13:06:08 +0800

    feat: post message mobile

--- src/button/example/ModifyColor.vue
+++ src/button/example/ModifyColor.vue
@@ -1,22 +1,21 @@
 <script setup>
 import { watchLang } from '@varlet/cli/site/utils'
-import { pack, use } from './locale'
+import { use } from './locale'
 import ButtonTheme from '../theme/button'
-import { Input as varInput } from '@varlet/ui'
+import { Input as VarInput } from '@varlet/ui'
 import '@varlet/ui/es/input/style/index'
 import { reactive, watch } from 'vue'
 import { DiffObj, Debounce } from '../../utils/diff'
 
 const obj = JSON.parse(JSON.stringify(ButtonTheme))
 const _ButtonTheme = reactive(ButtonTheme)
-watch(_ButtonTheme,
-  (newVal) => {
-    console.log(DiffObj(newVal, obj));
-  }
-)
+watch(_ButtonTheme, (newVal) => {
+  window[0].postMessage('theme_change', '*')
+  localStorage.setItem('button', JSON.stringify(DiffObj(newVal, obj)))
+})
 
 const handleChange = (e, key) => {
-  Debounce(()=> {
+  Debounce(() => {
     _ButtonTheme[key] = e.target.value
   }, 2000)()
 }
@@ -26,15 +25,19 @@ watchLang(use, 'pc')
 
 <template>
   <div class="body">
-    <div v-for="(val,key,i) in _ButtonTheme" :key="i">
+    <div v-for="(val, key, i) in _ButtonTheme" :key="i">
       <div v-if="key.includes('color')" style="margin-bottom: 24px">
-        <label class="text">{{key}}</label>
-        <input type="color" :value="_ButtonTheme[key]" style="display: block; width: 100%; margin-top: 8px;" @input="handleChange($event, key)">
+        <label class="text">{{ key }}</label>
+        <input
+          type="color"
+          :value="_ButtonTheme[key]"
+          style="display: block; width: 100%; margin-top: 8px"
+          @input="handleChange($event, key)"
+        />
       </div>
       <var-input class="label" v-else :placeholder="key" clearable v-model="_ButtonTheme[key]" />
     </div>
   </div>
-
 </template>
 
 <style>
--- src/button/example/index.vue
+++ src/button/example/index.vue
@@ -1,13 +1,115 @@
 <script setup>
-import AppType from '@varlet/cli/site/mobile/components/AppType'
-import { watchLang } from '@varlet/cli/site/utils'
-import BasicUse from './BasicUse'
+import { Button as VarButton, Space as VarSpace, Snackbar, Icon as VarIcon, StyleProvider } from '@varlet/ui'
 import { pack, use } from './locale'
+import { watchLang } from '@varlet/cli/site/utils'
+import AppType from '@varlet/cli/site/mobile/components/AppType'
+import '@varlet/ui/es/space/style/index'
+import '@varlet/ui/es/button/style/index'
+import { reactive } from 'vue'
+
+const VarStyleProvider = StyleProvider.Component
+
+const _ButtonTheme = reactive({value: {}})
+window.addEventListener('message', res => {
+  _ButtonTheme.value = JSON.parse(localStorage.getItem('button'))
+})
+
+const handleClick = () => {
+  Snackbar.success(pack.value.clickSuccess)
+}
+
+const handleTouchstart = () => {
+  Snackbar.success(pack.value.touchstartSuccess)
+}
+
+const handleAutoLoadingClick = () => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, 2000)
+  })
+}
+
 
 watchLang(use)
+// watchDarkMode(dark)
 </script>
 
 <template>
-  <app-type>{{ pack.basicUse }}</app-type>
-  <basic-use/>
+  <var-style-provider :style-vars="_ButtonTheme.value">
+    <app-type>{{ pack.themeColorButton }}</app-type>
+    <var-space :size="['2.666vw', '2.666vw']">
+      <var-button>{{ pack.defaultButton }}</var-button>
+      <var-button type="primary">{{ pack.primaryButton }}</var-button>
+      <var-button type="info">{{ pack.infoButton }}</var-button>
+      <var-button type="success">{{ pack.successButton }}</var-button>
+      <var-button type="warning">{{ pack.warningButton }}</var-button>
+      <var-button type="danger">{{ pack.dangerButton }}</var-button>
+    </var-space>
+
+    <app-type>{{ pack.textButton }}</app-type>
+    <var-space :size="['2.666vw', '2.666vw']">
+      <var-button text outline type="primary">{{ pack.outlineButton }}</var-button>
+      <var-button text type="primary">{{ pack.plainTextButton }}</var-button>
+    </var-space>
+
+    <app-type>{{ pack.disabledStatus }}</app-type>
+    <var-space :size="['2.666vw', '2.666vw']">
+      <var-button disabled>{{ pack.disabledStatus }}</var-button>
+      <var-button disabled text outline>{{ pack.disabledStatus }}</var-button>
+      <var-button disabled text>{{ pack.disabledStatus }}</var-button>
+    </var-space>
+
+    <app-type>{{ pack.loadingStatus }}</app-type>
+    <var-space :size="['2.666vw', '2.666vw']">
+      <var-button loading type="primary">{{ pack.loadingStatus }}</var-button>
+      <var-button loading type="info" loading-type="rect">{{ pack.loadingStatus }}</var-button>
+      <var-button loading type="success" loading-type="disappear">{{ pack.loadingStatus }}</var-button>
+      <var-button loading type="danger" loading-type="cube">{{ pack.loadingStatus }}</var-button>
+      <var-button loading type="warning" loading-type="wave">{{ pack.loadingStatus }}</var-button>
+    </var-space>
+
+    <app-type>{{ pack.buttonSize }}</app-type>
+    <var-space align="center" :size="['2.666vw', '2.666vw']">
+      <var-button type="primary">{{ pack.normalButton }}</var-button>
+      <var-button type="success" size="small">{{ pack.smallButton }}</var-button>
+      <var-button type="warning" size="mini">{{ pack.miniButton }}</var-button>
+      <var-button type="danger" size="large">{{ pack.largeButton }}</var-button>
+    </var-space>
+
+    <app-type>{{ pack.blockButton }}</app-type>
+    <var-button block type="primary">{{ pack.blockButton }}</var-button>
+
+    <app-type>{{ pack.customColor }}</app-type>
+    <var-space :size="['2.666vw', '2.666vw']">
+      <var-button color="#69dbaa" text-color="#eee">{{ pack.backgroundTextColor }}</var-button>
+      <var-button color="linear-gradient(to right, #69dbaa, #3a7afe)" text-color="#fff">
+        {{ pack.linearGradientColor }}
+      </var-button>
+    </var-space>
+
+    <app-type>{{ pack.roundButton }}</app-type>
+    <var-space :size="['2.666vw', '2.666vw']">
+      <var-button type="primary" round>
+        <var-icon name="plus" />
+      </var-button>
+      <var-button type="info" round>
+        <var-icon name="information" />
+      </var-button>
+      <var-button type="success" round>
+        <var-icon name="check" />
+      </var-button>
+      <var-button type="warning" round>
+        <var-icon name="warning" />
+      </var-button>
+      <var-button type="danger" round>
+        <var-icon name="window-close" />
+      </var-button>
+    </var-space>
+
+    <app-type>{{ pack.event }}</app-type>
+    <var-space :size="['2.666vw', '2.666vw']">
+      <var-button type="success" @click="handleClick">{{ pack.click }}</var-button>
+      <var-button type="success" @touchstart="handleTouchstart">{{ pack.touchstart }}</var-button>
+      <var-button type="success" @click="handleAutoLoadingClick" auto-loading>{{ pack.autoLoading }}</var-button>
+    </var-space>
+  </var-style-provider>
 </template>
