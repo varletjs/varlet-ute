8b07073bcf63f16536ad519e5ef6a0a92fe46cb5
haoziqaq <357229046@qq.com>
2022-09-28 01:11:53 +0800

    refactor: remove unuseful code

--- src/button/example/locale/dist/en-US.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-exports.__esModule = true
-exports['default'] = {
-  basicUse: 'Basic use',
-  modifyColor: 'Modify Color',
-  start: 'Start',
-}
--- src/button/example/locale/dist/zh-CN.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-exports.__esModule = true
-exports['default'] = {
-  basicUse: '基本使用',
-  modifyColor: '修改颜色',
-  start: '起步',
-}
