e72635c29c96935e17a6cba7a508c34bfa3d39e5
无措HOME <1015707397@qq.com>
2022-09-28 22:41:46 +0800

    feat(divider): add progress divider

---
+++ src/divider/example/index.vue
@@ -0,0 +1,57 @@
+<script setup>
+import AppType from '@varlet/cli/site/mobile/components/AppType'
+import { Divider as VarDivider, Icon as VarIcon } from '@varlet/ui'
+import '@varlet/ui/es/divider/style/index'
+import '@varlet/ui/es/icon/style/index'
+import dark from '../../../theme/dark'
+import { pack, use } from './locale'
+import { useSync } from '../../utils/sync'
+import { watchLang, watchDarkMode } from '@varlet/cli/site/utils'
+
+watchLang(use)
+watchDarkMode(dark)
+useSync()
+</script>
+<template>
+  <app-type>{{ pack.basicUsage }}</app-type>
+  <var-divider />
+
+  <app-type>{{ pack.dashed }}</app-type>
+  <var-divider dashed />
+
+  <app-type>{{ pack.inset }}</app-type>
+  <var-divider inset />
+  <var-divider :inset="36" margin="36px 0" />
+  <var-divider inset="-36px" />
+
+  <app-type>{{ pack.vertical }}</app-type>
+  <div class="divider-example-vertical-container">
+    <span>{{ pack.text }}</span>
+    <var-divider vertical />
+    <span>{{ pack.text }}</span>
+    <var-divider vertical />
+    <span>{{ pack.text }}</span>
+  </div>
+
+  <app-type>{{ pack.withDesc }}</app-type>
+  <var-divider :description="pack.withDescText" />
+
+  <app-type>{{ pack.custom }}</app-type>
+  <var-divider>
+    <var-icon name="heart-outline" style="margin: 0 16px; color: rgb(41, 121, 255)" />
+  </var-divider>
+</template>
+
+<style lang="less" scoped>
+.divider-example-vertical-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #333;
+
+  span {
+    font-size: 14px;
+    color: #888;
+  }
+}
+</style>
