2206e9759f278769118472dd5e4411044c925133
wzhaofei <wangzf@wellnkiot.com>
2022-09-28 17:56:21 +0800

    feat(swipe): add swipe component

---
+++ src/swipe/example/Editor.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { watchLang } from '@varlet/cli/site/utils'
+import { use } from './locale'
+import Editor from '../../../components/editor/index.vue'
+
+watchLang(use, 'pc')
+</script>
+
+<template>
+  <editor component-name="swipe" />
+</template>
