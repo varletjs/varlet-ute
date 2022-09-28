6aba9265097eeef9616dc82e178169ca680224d3
priority <1832052104@qq.com>
2022-09-28 08:51:31 +0800

    fix: icon file

--- src/icon/__tests__/index.spec.js
+++
@@ -1,7 +0,0 @@
-import Icon from '../index'
-import { createApp } from 'vue'
-
-test('test Icon use', () => {
-  const app = createApp({}).use(Icon)
-  expect(app.component(Icon.name)).toBeTruthy()
-})
