1fd3c981aeaf6712bc94b58acad7b7a1c353bc7f
wzhaofei <wangzf@wellnkiot.com>
2022-10-01 21:34:44 +0800

    feat(picker): add picker component

---
+++ src/picker/docs/en-US.md
@@ -0,0 +1,5 @@
+# Picker
+
+```vue
+import Editor from '../example/Editor'
+```
---
+++ src/picker/example/locale/en-US.ts
@@ -0,0 +1,9 @@
+export default {
+  functionCall: 'Function Call',
+  componentCall: 'Component Call',
+  singlePicker: 'Single Column Picker',
+  multiplePicker: 'Multiple Column Picker',
+  cascadePicker: 'Cascade Column Picker',
+  textFormatter: 'Text Formatter',
+  extension: 'Value Mapping',
+}
