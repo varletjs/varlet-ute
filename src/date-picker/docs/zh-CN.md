4ac6e3f4a47e8f72f18dc509d1e057109dd048d8
wzhaofei <wangzf@wellnkiot.com>
2022-10-02 18:24:17 +0800

    feat(date-picker): add date-picker component

---
+++ src/date-picker/docs/zh-CN.md
@@ -0,0 +1,5 @@
+# 倒计时
+
+```vue
+import Editor from '../example/Editor'
+```
---
+++ src/date-picker/example/index.vue
@@ -0,0 +1,74 @@
+<script setup>
+import { DatePicker as VarDatePicker } from '@varlet/ui'
+import { pack, use } from './locale/index'
+import AppType from '@varlet/cli/site/mobile/components/AppType'
+import '@varlet/ui/es/icon/style/index'
+import '@varlet/ui/es/cell/style/index'
+import { watchLang } from '@varlet/cli/site/utils'
+import { useSync } from '../../utils/sync'
+import { reactive } from 'vue'
+
+const dates = reactive({
+  date: '2021-04',
+  date1: '2021-04-08',
+  date2: ['', ''],
+  date3: ['2021-02-01', '2021-02-15'],
+  date4: '2020-11-11',
+  date5: '2021-05',
+})
+
+const allowedDates = (date) => {
+  return parseInt(date.split('-')[1], 10) % 2 === 1
+}
+const allowedDates1 = (date) => {
+  return parseInt(date.split('-')[2], 10) % 2 === 1
+}
+const change = (date) => {
+  console.log(date)
+}
+const preview = (year, month) => {
+  console.log(year, month)
+}
+
+watchLang(use)
+useSync()
+</script>
+
+<template>
+  <app-type>{{ pack.basicUsage }}</app-type>
+  <var-date-picker v-model="dates.date1" />
+
+  <app-type>{{ pack.monthPicker }}</app-type>
+  <var-date-picker type="month" v-model="dates.date" elevation />
+
+  <app-type>{{ pack.multiple }}</app-type>
+  <var-date-picker type="date" v-model="dates.date2" multiple />
+
+  <app-type>{{ pack.range }}</app-type>
+  <var-date-picker type="date" v-model="dates.date3" range />
+
+  <app-type>{{ pack.dateLimit }}</app-type>
+  <var-date-picker type="date" v-model="dates.date4" min="2020-10-15" max="2021-01-15" :allowed-dates="allowedDates1" />
+
+  <app-type>{{ pack.custom }}</app-type>
+  <var-date-picker
+    type="month"
+    :allowed-dates="allowedDates"
+    v-model="dates.date5"
+    max="2022-01"
+    min="2016-07"
+    elevation
+    header-color="purple"
+    color="#7bb872"
+    first-day-of-week="1"
+    @preview="preview"
+    @change="change"
+  >
+    <template #year="{ year }">
+      <span>{{ year }}{{ pack.year }}</span>
+    </template>
+    <template #month="{ year, month }">
+      <span>{{ year }}{{ pack.divider }}{{ month }}{{ pack.month }}</span>
+    </template>
+  </var-date-picker>
+</template>
