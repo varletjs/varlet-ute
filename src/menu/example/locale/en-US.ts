d1f2f2c9b044bc02910b4c0264abe949d3c7616c
wzhaofei <wangzf@wellnkiot.com>
2022-10-12 17:18:03 +0800

    feat(menu): add menu component

---
+++ src/menu/example/locale/en-US.ts
@@ -0,0 +1,12 @@
+export default {
+  basicUsage: 'Basic Usage',
+  alignmentMethods: 'Alignment Methods',
+  topAlignment: 'Top Alignment',
+  menuOption: 'Menu Option',
+  bottomAlignment: 'Bottom Alignment',
+  offset: 'Offset',
+  events: 'Events',
+  disabled: 'Disabled',
+  placement: 'Placement',
+  trigger: 'Trigger',
+}
---
+++ theme/light/menu.ts
@@ -0,0 +1,5 @@
+const MenuTheme = {
+  '--menu-background-color': '#272727',
+}
+
+export default MenuTheme
