1e8a2cf1af314b4c585e799d61a4fda11a5e5741
sdhushu <1426732052@qq.com>
2022-10-27 13:31:11 +0800

    feat: reset current properties

--- components/editor/index.vue
+++ components/editor/index.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import { computed, defineProps, onUnmounted, Ref, ref, watch } from 'vue'
+import { computed, defineProps, onMounted, onUnmounted, Ref, ref, watch } from 'vue'
 import { watchLang, watchTheme } from '@varlet/cli/site/utils'
 import {
   Input as VarInput,
@@ -33,6 +33,7 @@ type Theme = 'lightTheme' | 'darkTheme'
 
 const currentTheme: Ref<Theme> = ref((localStorage.getItem('VARLET_THEME') as Theme) ?? 'lightTheme')
 const model = ref(getModel())
+const originalTheme = ref()
 
 function getPatch(): any {
   const patch = JSON.parse(localStorage.getItem(currentTheme.value)!)
@@ -45,6 +46,10 @@ function getPatch(): any {
   const componentPatch = (patch ?? {})[props.componentName] ?? {}
 
   return { patch: patch ?? {}, componentPatch }
+}
+
+function getOriginalTheme() {
+  return currentTheme.value === 'darkTheme' ? { ...presetLightTheme, ...presetDarkTheme } : presetLightTheme
 }
 
 function getModel() {
@@ -93,7 +98,16 @@ watchTheme((theme) => {
 
   getPatch()
   model.value = getModel()
+  originalTheme.value = getOriginalTheme()
   StyleProvider(theme === 'lightTheme' ? null : flatObject(presetDarkTheme))
+})
+
+onMounted(() => {
+  Object.keys(model.value).forEach((key, index) => {
+    if (model.value[key] !== originalTheme.value[props.componentName][key]) {
+      editorItems.value[index].inputVisible.value = true
+    }
+  })
 })
 
 window.addEventListener('message', handleMessage)
@@ -101,7 +115,14 @@ onUnmounted(() => {
   window.addEventListener('message', handleMessage)
 })
 
-const editorItems = computed(() => Object.keys(model.value))
+const editorItems = computed(() => {
+  return Object.keys(model.value).map((key) => {
+    return {
+      key,
+      inputVisible: ref(false),
+    }
+  })
+})
 
 const clear = () => {
   const { patch } = getPatch()
@@ -172,12 +193,25 @@ const handleClick = async () => {
       : ''
     : ''
 }
+
+const handleRefresh = (model: any, key: { key: string; inputVisible: any }) => {
+  model[key.key] = originalTheme.value[props.componentName][key.key]
+  handleInput(model, key)
+}
+
+const handleInput = (model: any, key: { key: string; inputVisible: any }) => {
+  key.inputVisible.value = model[key.key] !== originalTheme.value[props.componentName][key.key]
+}
 </script>
 
 <template>
   <div class="editor" ref="body">
     <div v-for="key in editorItems" :key="key">
-      <var-input class="editor__input" :placeholder="key" v-model="model[key]"> </var-input>
+      <var-input class="editor__input" :placeholder="key.key" v-model="model[key.key]" @input="handleInput(model, key)">
+        <template v-if="key.inputVisible.value" #append-icon>
+          <var-icon name="refresh" @click="handleRefresh(model, key)" />
+        </template>
+      </var-input>
     </div>
 
     <var-button class="editor__suspend editor--375" type="primary" round @click="handleClick">
